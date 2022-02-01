dba0b863b35965a3f5aa335fe160130526e1996b
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-02-01 10:08:09 -0500

    added a score page and user input

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# vigilant-disco
+# Quiz
+
+## Purpose
+
+To use Javascript to create a dynamically generated quiz from an array of objects. Set that quiz to a timer and save high scores 
+
+## Technologies Used
+
+Javascript
+HTML
+CCSS
+
+## Plans for future development
+
+Get the quiz to change the questions to the next in the array
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,37 +4,39 @@ var questionsArr = [
     A:"Hey thats my leg!",
     B:"Hyper Test Make-Up Lozenge",
     C:"Hyper Text Mark-Up Language",
-    answer: "Hyper Text Mark-Up Language",
+  
   },
   {
     question: "How is a comment marked on an HTML page?",
     A:">>",
     B:"//",
     C:"++",
-    answer: "//",
+   
   },
   {
     question: "What are the primary programming languages for the front end of most applications?",
     A:"HTML, CSS, JavaScript",
     B:"Python, C++, Java",
     C:"HTTP, C++, JavaScript",
-    answer: "HTML, CSS, JavaScrpt",
+    
   },
   {
-    question: "",
-    A:"Hey thats my leg!",
-    B:"Hyper Test Make-Up Lozenge",
-    C:"Hyper Text Mark-Up Language",
-    answer: "4",
+    question: "What side of development deals with the user interface?",
+    A:"Front End Development",
+    B:"Back End Development",
+    C:"Dead End Development",
+  
   },
   {
-    question: "question 3",
-    A:"Hey thats my leg!",
-    B:"Hyper Test Make-Up Lozenge",
-    C:"Hyper Text Mark-Up Language",
-    answer: "4",
+    question: "What is the answer to life, the universe and everything?",
+    A:"Be good to one another,and leave the world a better place than when you came into it.",
+    B:"African or European Swallow?",
+    C:"42",
+  
   },
 ];
+
+var quizAnswers = ['Hyper Text Mark-Up Language', '//', 'HTML, CSS, JavaScript','Front End Development', '42'];
 var points = 0;
 var currentQuestionIndex = 0
 var i = 0
@@ -42,12 +44,21 @@ var containerEl = document.getElementById("questions")
 var choicesEl = document.getElementById("choices")
 var scoreEl = document.getElementById("score");
 var timerEl = document.getElementById("timer");
-var currentQuestionObj = questionsArr[currentQuestionIndex]
+var endScreenEl = document.querySelector(".end-screen")
+
 var nextQuestionEl = document.querySelector(".next-question")
 var nextBtnEl = document.querySelector(".next-btn")
 var titleEl = document.getElementById("title")
+
 //  gets the questions from the questions array
 var getQuestion = function() {
+
+  if(currentQuestionIndex === questionsArr.length) {
+
+    endGame()
+  } else {
+
+  var currentQuestionObj = questionsArr[currentQuestionIndex]
 
   var questionEl = document.createElement("div")
   questionEl.innerHTML = "<h2 class='question'>" + currentQuestionObj.question + "</h2>";
@@ -74,69 +85,32 @@ var getQuestion = function() {
 
   //increase the current question index by one for the next question
   currentQuestionIndex++;
+  console.log(currentQuestionIndex)
+  }
+};
 
-    };
 getQuestion();
+
 //  button handler for the answer buttons 
 var answerButtonHandler = function(event) {
  event.preventDefault();
   var targetEl = event.target;
+  var userAnswer = targetEl.textContent
 
-  if (targetEl.matches("#answer-a")) {
-    console.log("Button 1 was selected");
-    if (targetEl.textContent === currentQuestionObj.answer) {
+    if (quizAnswers.includes(userAnswer)) {
       console.log("correct!")
       targetEl.textContent = "Correct"
       points++
       console.log(points)
       targetEl.style.backgroundColor = "green"
-      containerEl.innerHTML = ""
-      getQuestion()
-      return
+
     } else {
       console.log("incorrect!")
       targetEl.textContent = "Incorrect!"
-      containerEl.innerHTML = ""
-      getQuestion()
-      return
+
     }
-  } else if (targetEl.matches("#answer-b")) {
-    console.log("Button 2 was selected");
-    if (targetEl.textContent === currentQuestionObj.answer) {
-      console.log("correct!")
-      targetEl.textContent = "Correct"
-      points++
-      console.log(points)
-      targetEl.style.backgroundColor = "green"
-      containerEl.innerHTML = ""
-      getQuestion()
-      return
-    } else {
-      console.log("incorrect!")
-      targetEl.textContent = "Incorrect!"
-      containerEl.innerHTML = ""
-      getQuestion()
-      return
-    }
-  } else if (targetEl.matches("#answer-c")) {
-    console.log("Button 3 was selected")
-    if (targetEl.textContent === currentQuestionObj.answer) {
-      console.log("correct!")
-      targetEl.textContent = "Correct"
-      points++
-      console.log(points)
-      targetEl.style.backgroundColor = "green"
-      containerEl.innerHTML = ""
-      getQuestion()
-      return
-    } else {
-      console.log("incorrect!")
-      targetEl.textContent = "Incorrect!"
-      containerEl.innerHTML = ""
-      getQuestion()
-      return
-    }
-  }
+    containerEl.innerHTML = ""
+    getQuestion()
 };
 console.log(currentQuestionIndex)
  var nextButtonHandler = function(event) {
@@ -148,6 +122,29 @@ console.log(currentQuestionIndex)
    console.log(currentQuestionObj.question)
    return 
  }
+
+var endGame = function() {
+  var score = document.createElement("div")
+  score.innerHTML = "<h1 class= 'score'> Your score is " + points +"!</h1>"
+  endScreenEl.appendChild(score)
+  userInput()
+ }
+var userInput = function() {
+ var initials = document.createElement("div")
+ initials.innerHTML = "<h2>Please enter your initials<input type='text'id = 'initials'></h2>";
+ endScreenEl.appendChild(initials)
+}
+
+
+var highScore = function(){
+ var UserScore = {
+   user: userInput.value,
+   score: points
+ }
+
+ localStorage.setItem("high-score", JSON.stringify(UserScore))
+
+}
 // Timer function 
 //  var clock = 1000
 //  var countDown = setInterval(function () {
@@ -159,11 +156,9 @@ console.log(currentQuestionIndex)
 //      return;
 //    }
 //  }, 1000);
- // Score at the top of the screen 
- scoreEl.textContent = "Current Score: " + points;
 //  Event listener for the answer buttons 
 containerEl.addEventListener("click", answerButtonHandler);
 //  Event listener for the next button
 nextQuestionEl.addEventListener("click", nextButtonHandler);
 
-  var highScores = []
+endScreenEl.addEventListener("submit", highScore)
